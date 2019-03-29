77d1a60bbe011b8249a0c5bef4fc5d199b21d986
Albert <m26416113@John.pertra.ac.id>
2019-03-29 23:31:35 +0700

    a

---
+++ includes/header.inc.php
@@ -0,0 +1,8 @@
+<?php   
+    include_once 'dbh.inc.php';
+    session_start();
+?>
+<nav>
+
+</nav>
+
---
+++ maintenance.php
@@ -0,0 +1,9 @@
+<?php
+    include_once 'includes/dbh.inc.php';
+    $cek_mtc = 'SELECT config_value FROM t_config WHERE config_id=1;';
+    $cek_mtc_r = mysqli_fetch_assoc(mysqli_query($conn,$cek_mtc))['config_value'];
+    if($cek_mtc_r==0){
+        header('Location: /manpro/index.php');
+    }
+?>
+<h1>Maaf Kami Sedang Melakukan Perbaikan</h1>
