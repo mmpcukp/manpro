6a58a2198d7360ec7806d2b6449ff28dca721bcc
Albert <m26416113@John.pertra.ac.id>
2019-03-30 09:06:00 +0700

    All

---
+++ includes/footer.inc.php
@@ -0,0 +1,3 @@
+<?php
+?>
+Ini Adalah Footer
--- index.php
+++ index.php
@@ -6,18 +6,20 @@
         header('Location: /manpro/maintenance.php');
     }
 ?>
-<!--Main Text-->
-<h1>
+<!--Main OBJECT-->
+<h1 style="text-align: center">
 <?php
     $main_txt   = 'SELECT config_value FROM t_config WHERE config_id=2;';
     $main_txt_r = mysqli_fetch_assoc(mysqli_query($conn,$main_txt))['config_value'];
     echo $main_txt_r;
 ?>
 </h1>
-<!--End Of Main Text-->
-
 <?php
     $main_img   = 'SELECT config_value FROM t_config WHERE config_id=3;';
     $main_img_r = mysqli_fetch_assoc(mysqli_query($conn,$main_img))['config_value'];
-    
+    echo '<img src="'.$main_img_r.'">';
+?>
+<!--End Of Main OBJECT-->
+<?php
+    include_once 'includes/footer.inc.php';
 ?>
